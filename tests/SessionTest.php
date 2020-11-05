87f3612e327a2818517ad8b84ba7ec89541fa7d4
Adrien Antonutti <antonuttiadrien@gmail.com>
2020-11-05 13:39:23 +0100

    Migration to php 7.4, phpunit9 and twig3

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -6,7 +6,7 @@ use bemang\Session\ArraySession;
 
 class SessionTest extends \PHPUnit\Framework\TestCase
 {
-    public function setUp()
+    public function setUp() :void
     {
         require_once(__DIR__ . '/../vendor/autoload.php');
     }
